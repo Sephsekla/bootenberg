8ffb20da059913fa6115bf0b03895b69e7860339
Joe Bailey-Roberts <dev@joebr.io>
2020-10-20 15:13:40 +0100

    Edit readme and plugin file

--- plugin.php
+++ plugin.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Plugin Name: bootenberg — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: bootenberg — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: Bootenberg
+ * Plugin URI: https://github.com/sephsekla/bootenberg
+ * Description: Bringing the power of Bootstrap 4 to the Gutenberg Editor
+ * Author: Joe Bailey-Roberts
+ * Author URI: https://jbr.digital
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
