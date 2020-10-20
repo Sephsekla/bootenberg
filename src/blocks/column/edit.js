f2ef5741ea7c41f3ffe6fc4d3e03ca91f91ba93e
Joe Bailey-Roberts <dev@joebr.io>
2020-10-20 14:31:05 +0100

    Separate select component

--- src/blocks/column/edit.js
+++ src/blocks/column/edit.js
@@ -6,6 +6,44 @@ const classnames = require( 'classnames' );
 const { Fragment } = wp.element;
 const { createHigherOrderComponent } = wp.compose;
 
+const RenderSelect = ( props ) => {
+
+    const { size, label, label2 } = props;
+
+    const { className, setAttributes, onUpdate } = props;
+    const { xs, sm, md, lg, xl } = props.attributes;
+
+    return <PanelRow>
+    <label htmlFor='reverse-form-toggle'>
+        { label }
+    </label>
+     <SelectControl
+        id='reverse-form-toggle'
+        label={ label2 }
+        value={ size }
+        onChange={ onUpdate }
+
+        
+        options={ [
+            //{ value: null, label: 'Select a User', disabled: false },
+            { value: 1, label: '1 (1/12)' },
+            { value: 2, label: '2 (1/6)' },
+            { value: 3, label: '3 (1/4)' },
+            { value: 4, label: '4 (1/3)' },
+            { value: 5, label: '5 (5/12)' },
+            { value: 6, label: '6 (1/2)' },
+            { value: 7, label: '7 (7/12)' },
+            { value: 8, label: '8 (2/3)' },
+            { value: 9, label: '9 (3/4)' },
+            { value: 10, label: '10 (5/6)' },
+            { value: 11, label: '11 (11/12)' },
+            { value: 12, label: '12 (Full-width)' },
+        ] } 
+        
+    />
+</PanelRow>
+}
+
 const editColumn = ( props ) => {
     const { className, setAttributes } = props;
     const { xs, sm, md, lg, xl } = props.attributes;
@@ -13,36 +51,8 @@ const editColumn = ( props ) => {
     return (
         <Fragment>
             <InspectorControls>
-                 <PanelBody  title={ 'About' } >
-                    <PanelRow>
-                        <label htmlFor='reverse-form-toggle'>
-                            { 'Width' }
-                        </label>
-                         <SelectControl
-                            id='reverse-form-toggle'
-                            label={ 'Default' }
-                            value={ xs }
-                            onChange={ ( value ) => setAttributes( { xs: parseInt(value) } ) }
-
-                            
-                            options={ [
-                                //{ value: null, label: 'Select a User', disabled: false },
-                                { value: 1, label: '1 (1/12)' },
-                                { value: 2, label: '2 (1/6)' },
-                                { value: 3, label: '3 (1/4)' },
-                                { value: 4, label: '4 (1/3)' },
-                                { value: 5, label: '5 (5/12)' },
-                                { value: 6, label: '6 (1/2)' },
-                                { value: 7, label: '7 (7/12)' },
-                                { value: 8, label: '8 (2/3)' },
-                                { value: 9, label: '9 (3/4)' },
-                                { value: 10, label: '10 (5/6)' },
-                                { value: 11, label: '11 (11/12)' },
-                                { value: 12, label: '12 (Full-width)' },
-                            ] } 
-                            
-                        />
-                    </PanelRow>
+                 <PanelBody  title={ 'Width' } >
+                    <RenderSelect size={xs} label={'Width'} label2={'Default'} onUpdate={ ( value ) => setAttributes( { xs: parseInt(value) } ) } {...props} />
                 </PanelBody>
             </InspectorControls>
 
