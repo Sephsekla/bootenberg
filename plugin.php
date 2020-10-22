5792bd5f6cc7ec12cfc2ab6d89c4bf19566b9f28
Joe Bailey-Roberts <dev@joebr.io>
2020-10-22 16:54:11 +0100

    Update readme etc

--- plugin.php
+++ plugin.php
@@ -1,13 +1,18 @@
 <?php
 /**
- * Plugin Name: Bootenberg
- * Plugin URI: https://github.com/sephsekla/bootenberg
- * Description: Bringing the power of Bootstrap 4 to the Gutenberg Editor
- * Author: Joe Bailey-Roberts
- * Author URI: https://jbr.digital
- * Version: 1.3.1
- * License: GPL2+
- * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *  Plugin Name: Bootenberg
+ *  Plugin URI: https://github.com/sephsekla/bootenberg
+ *  Description: Bringing the power of Bootstrap 4 to the Gutenberg Editor
+ *  Author: Joe Bailey-Roberts
+ *  Author URI: https://jbr.digital
+ *  Version: 1.3.1
+ *  Stable tag: Trunk
+ *  Tags: Gutenberg, Layout, Bootstrap
+ *  Requires at least: 5.5
+ *  Tested up to: 5.5
+ *  License: GPL2+
+ *  License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+
  *
  * @package bootenberg
  */
